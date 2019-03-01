a90f2441b89603cd3394c2aa8846f46e0e081743
asamnt <37043202+asamnt@users.noreply.github.com>
2019-03-02 01:54:55 +0530

    Add files via upload

--- dict_to_csv.py
+++ dict_to_csv.py
@@ -1,11 +1,13 @@
 import json
 import csv
 import requests
+import datetime
 
 def load_dict_to_csv():
 
     #response = request.GET('https://jsonplaceholder.typicode.com/posts')
     #print(json.loads(response.text))
+
 
     response = requests.get("https://jsonplaceholder.typicode.com/todos")
     todos = json.loads(response.text)
@@ -24,4 +26,20 @@ def load_dict_to_csv():
 
 
 if __name__ == '__main__':
-    load_dict_to_csv()
+    #load_dict_to_csv()
+    from_date = datetime.datetime(2019, 1, 1)
+    to_date = datetime.datetime(2019, 1, 5)
+    datediff=(to_date-from_date).days
+    print(datediff)
+    print('date diff'+str(datediff))
+    print(from_date.timestamp())
+    print(round(to_date.timestamp()))
+    print(type(from_date))
+
+
+    while datediff>0:
+        print(datediff)
+        datediff=datediff-1
+
+    timestamp = datetime.datetime(2019, 3, 1, 0, 0).timestamp() * 1000
+    print(round(timestamp))
